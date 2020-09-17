4c90a84533f8c2a6c18e110d7f1fad8a585cfffa
e <18607005537@163.com>
2020-09-17 20:07:47 +0800

    完善模块化

--- js基础/es6/babel/lib/test.js
+++ js基础/es6/babel/lib/test.js
@@ -1,17 +1,22 @@
-var count = 3
+// var count = 3
 
 
-function add () {
-    count++
-}
-// node
-// module.exports = {
-//     count,
-//     add
+// function add () {
+//     count++
+// }
+// // node
+// // module.exports = {
+// //     count,
+// //     add
+// // }
+
+// //es6
+// export {
+//   count,
+//   add
 // }
 
-//es6
-export {
-  count,
-  add
-}
+import {bar} from './test1.js';
+console.log('a.js');
+console.log(bar);
+export let foo = 'foo';
--- js基础/es6/babel/lib/test1.js
+++ js基础/es6/babel/lib/test1.js
@@ -1,8 +1,13 @@
 // node
 // var a = require('./test')
 //es6
-import * as a from './test'
-console.log(a)
-console.log(a.count);  // 3
-a.add();
-console.log(a.count); // es6 4 node 3
+// import * as a from './test'
+// console.log(a)
+// console.log(a.count);  // 3
+// a.add();
+// console.log(a.count); // es6 4 node 3
+
+import {foo} from './test.js';
+console.log('b.js');
+console.log(foo);
+export let bar = 'bar';
